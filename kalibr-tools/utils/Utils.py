b82ef769a8803ff47ae588d97c179958e5f0c287
nlpng <neil.phuang@gmail.com>
2024-08-28 21:26:17 +0900

    add shell scripts generator for calibration

---
+++ kalibr-tools/utils/Utils.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rosbag
+import os
+
+
+def splitImage(img, num_subimages=4):
+    # Split image vertically
+    _, w = img.shape[:2]
+    sub_w = w // num_subimages
+    sub_imgs = []
+    for i in range(num_subimages):
+        sub_imgs.append(img[:, i * sub_w : (i + 1) * sub_w])
+    return sub_imgs
+
+
+def check_rosbag(image_topic, imu_topic, bag_path):
+    if not os.path.exists(bag_path):
+        print("Bag file:{} does not exist".format(bag_path))
+        return False
+
+    bag = rosbag.Bag(bag_path)
+    if bag.get_message_count(image_topic) <= 0:
+        print(
+            "No message in topic: {} check configuration".format(image_topic)
+        )
+        bag.close()
+        return False
+
+    if bag.get_message_count(imu_topic) <= 0:
+        print(
+            "No message in topic: {} check configuration".format(imu_topic)
+        )
+        bag.close()
+        return False
+    bag.close()
+    return True
