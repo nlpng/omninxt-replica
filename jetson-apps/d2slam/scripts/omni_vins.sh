1795b9574452940bc0aa98e150876d5ddd84f0a7
nlpng <neil.phuang@gmail.com>
2025-01-20 23:19:55 +0900

    add xhost for dispaly and fix indent

--- jetson-apps/d2slam/scripts/omni_vins.sh
+++ jetson-apps/d2slam/scripts/omni_vins.sh
@@ -6,7 +6,7 @@ docker run -it --rm --privileged \
         --runtime nvidia \
         --gpus all \
         --env="DISPLAY=:0" \
-	--env="QT_X11_NO_MITSHM=1" \
+	    --env="QT_X11_NO_MITSHM=1" \
         -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
         -v /dev:/dev \
         --name=omni-vins d2slam:noetic-r35.4.1 bash -c 'roslaunch d2vins vins-node.launch'
